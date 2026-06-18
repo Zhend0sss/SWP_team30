6fcc8f857eb92c747a83ca6e80b2f9a7394c2991
Son-Go <gorelovartemij@gmail.com>
2026-06-18 17:09:13 +0300

    feature: edit UserRepository

--- backend/gde_website/src/main/java/gde/gde_website/users/repository/UsersRepository.java
+++ backend/gde_website/src/main/java/gde/gde_website/users/repository/UsersRepository.java
@@ -1,7 +1,15 @@
 package gde.gde_website.users.repository;
 
+import gde.gde_website.users.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class UsersRepository {
+public interface UsersRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
